2b860f5132181435fbca47ebd54f27eb151394a1
Daniel Kuffner <chilicat.dev@gmail.com>
2021-10-24 14:25:10 +0200

    fix example: use new module name

--- examples/aks-centos-vmss/main.tf
+++ examples/aks-centos-vmss/main.tf
@@ -1,9 +1,8 @@
 
-module "centos7-vmss" {
-  source  = "chilicat/centos7-vmss/azurerm"
+module "aks-centos7-vmss" {
+  source  = "chilicat/aks-centos7-vmss/azurerm"
   version = ">0"
   resource_group_name = var.resource_group_name
   aks_name            = var.aks_name
   name                = var.name
 }
-
